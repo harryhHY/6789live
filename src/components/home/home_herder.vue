c3c770bc4a5228eedc0db8eb9be65a2f5e1719d7
harry1050732955 <harry1050732955@gmail.com>
2020-10-14 17:31:22 +0800

    制作首页头部

--- src/components/home/home_herder.vue
+++ src/components/home/home_herder.vue
@@ -1,27 +1,128 @@
 <template>
-    <div id="home_header" class="cl">
-        <h1 @click="go_homepage()" class="logo left">
-            <img src="" alt="" >
-        </h1>
+  <div id="home_header" class="cl">
+    <h1 @click="gotosm('/')" class="logo left"></h1>
+    <div class="menu left">
+      <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#ffffff"
+        text-color="#000"
+        active-text-color="#2a88f3"
+      >
+        <el-menu-item
+          v-for="(item, key, index) in title_data"
+          :key="index + item.index"
+          :index="item.index"
+        >
+          <i :class="item.icon"></i>{{ item.title }}</el-menu-item
+        >
+      </el-menu>
     </div>
+    <div class="login_div">
+      <button v-if="!token" class="loginbtn">登陆</button>
+      <div v-if="token">{{ namely }}</div>
+      <div class="span_div">
+        <span class="logon_span" v-if="!token" @click="gotosm('/registered')"
+          >还没注册？</span
+        >
+        <span class="logon_span" v-if="!token" @click="gotosm('/registered')"
+          >|　忘记密码</span
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            go_homepage() {
-                this.$router.push('/')
-            }
+export default {
+  data() {
+    return {
+      token: false,
+      activeIndex2: "1",
+      title_data: [
+        {
+          icon: "el-icon-house",
+          index: "1",
+          title: "首页",
         },
-    }
+        {
+          index: "2",
+          title: "直播",
+        },
+        {
+          index: "3",
+          title: "新闻",
+        },
+        {
+          index: "4",
+          title: "社区",
+        },
+      ],
+    };
+  },
+  methods: {
+    gotosm(src) {
+      this.$router.push(src);
+    },
+    handleSelect(key, keyPath) {
+      this.$emit("changetype", key);
+    },
+  },
+  created() {},
+};
 </script>
 
 <style lang="less" scoped>
-    #home_header{
-        width: 100%;
-        background-color: #f00;
-        .logo{
-
-        }
+#home_header {
+  width: 100%;
+  //   background-color: #f00;
+  .logo {
+    width: 15%;
+    height: 84px;
+    background: url("../../../static/image/logo.png") no-repeat;
+    background-size: 100%;
+    cursor: pointer;
+    image {
+      width: 100%;
+      height: 100%;
     }
+  }
+
+  .menu {
+    width: 65%;
+    ul {
+      margin-top: 20px;
+      .el-menu--horizontal {
+        border: none;
+      }
+      .el-menu-item:hover {
+        background: #ffffff !important;
+      }
+    }
+  }
+  .login_div {
+  }
+  .loginbtn {
+    color: #ffffff;
+    border: none;
+    font-size: 18px;
+    background-color: rgb(42, 136, 243);
+    padding: 15px 20px;
+    border-radius: 5px;
+    margin-left: 20px;
+    margin-top: (81px- 54px)/2;
+  }
+  .span_div {
+    margin-left: 20px;
+    display: inline-block;
+  }
+  .logon_span {
+    color: #777777;
+  }
+  .logon_span:hover {
+    cursor: pointer;
+  }
+}
 </style>
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,17 +1,79 @@
 <template>
   <div id="home">
-    <home_herder></home_herder>
+    <home_herder @changetype="parentEvent"></home_herder>
+    <div class="swp">
+      <el-carousel height="300px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 class="small">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
   </div>
 </template>
 
 <script>
 const home_herder = () => import("../../components/home/home_herder");
 export default {
+  data() {
+    return {
+      menu_num: "1",
+      swiperdata: [
+        {
+          id: 1,
+        //   src: require("../../../static/image/logo"),
+        },
+      ],
+    };
+  },
+  methods: {
+    parentEvent(data) {
+      this.menu_num = data;
+    },
+  },
+  watch: {
+    menu_num(newValue, oldValue) {
+      switch (newValue) {
+        case "1":
+          console.log("这个是1");
+          break;
+        case "2":
+          console.log("这个是2");
+          break;
+        case "3":
+          console.log("这个是3");
+          break;
+        case "4":
+          console.log("这个是4");
+          break;
+      }
+    },
+  },
   components: {
     home_herder,
   },
+  created() {},
 };
 </script>
 
 <style lang="less" scoped>
+.swp {
+  width: 65%;
+  margin-left: 7%;
+  height: 500px;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
 </style>
