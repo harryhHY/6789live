c9f33a17861b893850f13c0a4543dab8cd6e2fe6
kyle <runner_tong@sina.com>
2020-10-16 18:42:27 +0800

    联赛数据作废

--- src/page/leagepoints/leagepoints.vue
+++ src/page/leagepoints/leagepoints.vue
@@ -1,49 +1,52 @@
 <template>
-    <div>
-        <home_herder @changetype="parentEvent"></home_herder>
-        <component :is="component_name"></component>
+    <div id="leagepoints" class="cl">
+        <home_herder @changetype="parentEvent" :headerKey='headerKey'></home_herder>
+        <leagemenu  @changetype="gotmenutype"/>
+        <pointlist></pointlist>
     </div>
 </template>
 
 <script>
 const home_herder = () => import("../../components/home/home_herder");
-const home_content = () => import("../../components/home/home_content");
-const live = () => import("../../components/live/live");
+const leagemenu = () => import("../../components/leage/leagemenu");
+const pointlist = () => import("../../components/leage/pointlist");
 export default {
     name:"leagepoints",
     components: {
         home_herder,
-        home_content,
-        live,
+        leagemenu,
+        pointlist
     },
     data(){
         return{
-            menu_num: "",
-            component_name: "",
+            headerKey:'1'
         }
     },
     methods: {
         parentEvent(data) {
         this.menu_num = data;
         },
+        gotmenutype(type){
+            console.log(type);
+        }   
     },
     watch: {
-        menu_num(newValue, oldValue) {
-        switch (newValue) {
-            case "1":
-            return (this.component_name = "home_content");
-            break;
-            case "2":
-            return (this.component_name = "live");
-            break;
-            case "3":
-            //   return (this.component_name = "live");
-            break;
-            case "4":
-            console.log("这个是4");
-            break;
-        }
-        },
+        // menu_num(newValue, oldValue) {
+        //     switch (newValue) {
+        //         case "1":
+        //         return (this.component_name = "home_content");
+        //         break;
+        //         case "2":
+        //         return (this.component_name = "live");
+        //         break;
+        //         case "3":
+        //         //   return (this.component_name = "live");
+        //         break;
+        //         case "4":
+        //         console.log("这个是4");
+        //         break;
+        //     }
+        // },
     },
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,10 @@ const Login = import('../page/login/login.vue');
 const Registered = import('../page/registered/registered.vue');
 const Resetpass = import('../page/resetpass/resetpass.vue');
 const Home = import('../page/home/home.vue');
-const Leagepoints = import('../page/leagepoints/leagepoints.vue');
+// const Leagepoints = import('../page/leagepoints/leagepoints.vue');
 const Live = import('../components/live/live');
 const Livedel = import('../components/live/livedel.vue');
+const News = import('../page/news/news.vue');
 //获取原型对象上的push函数
 const originalPush = Router.prototype.push
 //修改原型对象中的push方法
@@ -46,11 +47,13 @@ export default new Router({
       name: 'resetpass',
       component: () => Resetpass
     },
-    {
-      path: '/leagepoints',
-      name: 'leagepoints',
-      component: () => Leagepoints
-    },
+
+    //联赛数据废弃
+    // {
+    //   path: '/leagepoints',
+    //   name: 'leagepoints',
+    //   component: () => Leagepoints
+    // },
 
   ]
 })
