d0df0dc742ade30bbf5fa61cc8272a0a00f7acff
harry1050732955 <harry1050732955@gmail.com>
2020-10-20 13:02:56 +0800

    数据分析

---
+++ src/components/analysis/analysis.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="dataanalysis_header">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item
+        :index="item.id"
+        v-for="(item, index) in dataList"
+        :key="index"
+        >{{ item.title }}</el-menu-item
+      >
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "2",
+      dataList: [
+        {
+          id: "1",
+          title: "直播间",
+          src: "/livedel",
+        },
+        {
+          id: "2",
+          title: "数据分析",
+          src: "/analysis",
+        },
+        {
+          id: "3",
+          title: "指数",
+          src: "/exponent",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+     for(let i =0;i<this.dataList.length;i++){
+         
+     }
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.dataanalysis_header{
+    display: flex;
+    justify-content: center;
+}
+</style>
--- src/page/analysis/analysis.vue
+++ src/page/analysis/analysis.vue
@@ -1,15 +1,25 @@
 <template>
-    <div class="dataanalysis">
-
-    </div>
+  <div class="dataanalysis">
+      <analyheader></analyheader>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+const analyheader = ()=>import('../../components/analysis/analysis.vue')
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+  components: {
+      analyheader,
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
